693851098f48c9b7e0d172dc88f08aaae2ea6a29
himitery <himit0131@gmail.com>
2022-11-07 02:37:48 +0900

    style(store): 상품 상세 페이지 하단 padding 디자인 수정

--- src/screens/store/product/StoreProductDescriptionScreen.tsx
+++ src/screens/store/product/StoreProductDescriptionScreen.tsx
@@ -1,17 +1,25 @@
-import React, { useCallback, useEffect } from 'react';
-import { ListRenderItem, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useCallback, useEffect, useMemo } from 'react';
+import {
+  FlatList,
+  ListRenderItem,
+  Platform,
+  StatusBar,
+  StyleProp,
+  StyleSheet,
+  ViewStyle,
+} from 'react-native';
 import { MaterialTopTabNavigationOptions } from '@react-navigation/material-top-tabs';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 import {
   ProductTopTabNavigation,
   ProductTopTabParamProps,
 } from '@/navigations/tab/product';
 import { File } from '@/domain/file';
-import StoreProductDescriptionItem from '@/screens/store/product/components/StoreProductDescriptionItem';
 import { GET_PRODUCT } from '@/operations/product/query/GetProduct';
-import { Colors } from '@/constants/color';
-import Animated from 'react-native-reanimated';
 import { productDetailHeaderHeightVar } from '@/stores/product';
+
+import StoreProductDescriptionItem from '@/screens/store/product/components/StoreProductDescriptionItem';
 
 export const StoreProductDescriptionScreenOptions: MaterialTopTabNavigationOptions =
   {
@@ -36,6 +44,16 @@ const StoreProductDescriptionScreen: React.FC<
     getProduct();
   }, []);
 
+  const bottomPaddingStyle = useMemo<StyleProp<ViewStyle>>(
+    () => ({
+      paddingBottom: Platform.select({
+        android: StatusBar.currentHeight,
+        ios: getBottomSpace(),
+      }),
+    }),
+    []
+  );
+
   const keyExtractor = useCallback((item: File) => item.id, []);
 
   const renderItem = useCallback<ListRenderItem<File>>(
@@ -56,26 +74,21 @@ const StoreProductDescriptionScreen: React.FC<
   }, []);
 
   return (
-    <SafeAreaView style={styles.safeContainer}>
-      <Animated.FlatList
-        style={styles.container}
-        data={data?.product?.description ?? []}
-        keyExtractor={keyExtractor}
-        renderItem={renderItem}
-        onScrollEndDrag={handleOnScrollEndDrag}
-        onScrollBeginDrag={handleOnScrollBeginDrag}
-        showsVerticalScrollIndicator={false}
-        bounces={false}
-      />
-    </SafeAreaView>
+    <FlatList
+      style={styles.container}
+      contentContainerStyle={bottomPaddingStyle}
+      data={data?.product?.description ?? []}
+      keyExtractor={keyExtractor}
+      renderItem={renderItem}
+      onScrollEndDrag={handleOnScrollEndDrag}
+      onScrollBeginDrag={handleOnScrollBeginDrag}
+      showsVerticalScrollIndicator={false}
+      bounces={false}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  safeContainer: {
-    flex: 1,
-    backgroundColor: Colors.white,
-  },
   container: {
     flex: 1,
   },
